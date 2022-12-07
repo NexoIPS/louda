a5e2bd697983aca72b372b8f9254dc185020df60
NexoIPS <57940813+NexoIPS@users.noreply.github.com>
2022-12-07 09:36:41 +0100

    komit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const port = 4000;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+let isActive = false;
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+
+app.get('/status', (req, res) =>{
+
+res.send({isActive});
+});
+
+app.post('/change', (req, res) =>{
+isActive = !isActive;
+res.send({isActive});
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+  });
